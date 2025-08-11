5e33b9d1918f153e2867984fa0ee4f9f6701940c
Abhay_0020 <abhaybhadauria8999@gmail.com>
2025-08-11 11:03:33 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        int hellB=89;
+        int _bh=90;
+        int $rc=890;
+        int sum = hellB + _bh;
+        System.out.println(sum);
+        
+
+    }
+    
+}
